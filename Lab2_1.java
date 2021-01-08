36ba24095d89c0d7a39e9a00923c7ec9d9e81239
Eugene Grunin <zhenechka_grunin@mail.ru>
2021-01-08 21:26:31 +0700

    Add files via upload

---
+++ Lab2_1.java
@@ -0,0 +1,18 @@
+package Lab2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1
+ */
+public class Lab2_1 {
+    public static void main(String[] args) { 
+        System.out.println ("а=1, c=a+1 m=2, n=3 решение уравнений");
+        System.out.println ("b=6, d=b*2-1 p=4, g=5 нахождение корней"); 
+}
+}
---
+++ Lab2_2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab2;
+
+/**
+ *
+ * @author 1
+ */
+public class Lab2_2 {
+    public static void main(String[] args) {
+        int a=1;
+     
+    System.out.print (" " + a); 
+    System.out.println (" "); 
+    for (int b = 1; b<=3; b++){ 
+    System.out.print (" " + b);} 
+    System.out.println (" "); 
+    for (int c = 1; c<=5; c++){ 
+    System.out.print (" " + c);} 
+    System.out.println (" "); 
+    for (int d = 1; d<=7; d++)
+    { System.out.print (" " + d);} 
+}
+    }
